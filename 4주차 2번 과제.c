48a66ecfd3a1375d82d31f2541b045cdbc01a10c
seaspis <163992015+seaspis@users.noreply.github.com>
2024-04-18 21:34:28 +0900

    Create 4주차 2번 과제.c

---
+++ 4주차 2번 과제.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	int a = 0, num[9], index;
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &num[i]);
+		if (a < num[i]) {
+			a = num[i];
+			index = i;
+		}
+	}
+	printf("%d\n%d", a, index + 1);
+}
